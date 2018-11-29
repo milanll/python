abd8dc70c1da6c83076b5d91c34e791c4ace79ef
milanll <andry_chen@qq.com>
2018-11-29 20:43:00 +0800

    update

--- py_code/GUI/read_word_txt.py
+++ py_code/GUI/read_word_txt.py
@@ -17,7 +17,7 @@ def get_words_dict(file):
     try:
         f = open(file, 'r', encoding = 'utf-8')
         reader = csv.reader(f)
-        flag_not_first = False
+        flag_first = True
         # 0     1         2         3               4                       5                   6
         #1-1,  unit-4,	课文,		1,		秋气了树叶片大飞会个,		了子大人,		好了-行了-走了-子女-日子-儿子-大人-人手-人们-大火-大手-大小
         for l in reader:
@@ -29,7 +29,7 @@ def get_words_dict(file):
             dict_4['田字格字'] = l[5]
             
             l[6] = l[6].strip().strip('\t')
-            dict_4['扩词表字'] = l[6]
+            dict_4['扩词表'] = l[6]
 
             
             #第几课
@@ -45,7 +45,9 @@ def get_words_dict(file):
 
                 #读取完一个单元的数据后，向dict_1更新一次。
                 #j==0时是第一次读取，不用向dict_1更新。
-                if flag_not_first:
+                if flag_first:
+                    flag_first = False
+                else:
                     dict_2_temp = deepcopy(dict_2)
                     
                     #年级-学期
@@ -54,9 +56,7 @@ def get_words_dict(file):
                         dict_1[l[0]] = dict_2_temp
                     else:
                         dict_1[l[0]].update(dict_2_temp)
-                
-                flag_not_first = True 
-                
+
                 dict_2[l[1]] = dict_3_temp
                 
             else:
@@ -75,9 +75,8 @@ def get_words_dict(file):
 if __name__ == '__main__':
 
     dict = get_words_dict('word.txt')
-
-    for (k, v) in dict['1-1'].items():
-        for (m, n) in v.items():
-            print(k, m, n)
+    
+    for e in dict['1-1']['unit-4'].items():
+        print(e)
 
     
