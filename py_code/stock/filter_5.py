4ba181213684da0e085485ec78b90d37c7eab408
milanll <andry_chen@qq.com>
2020-04-01 10:29:28 +0800

    update

--- py_code/stock/filter_5.py
+++ py_code/stock/filter_5.py
@@ -38,7 +38,9 @@ def filter_5(stock_data):
             stock_key.append(k)
             #print(k)
             progress_bar(j, base)
-            
+    print('''\n======================= 需求5 =============================
+            需求：  a.过去5个交易日中，有三个涨停板
+          ''')          
     stock_p_change = get_stock_info_by_key(stock_key)
     #save_stock(stock_p_change, '1_filter') 
 
